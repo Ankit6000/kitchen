3051efe2b646e72956abab0d00a3f517dbf7b233
elbicho-07 <al2346@srmist.edu.in>
2026-03-25 22:14:45 +0530

    Use repo base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
+  const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
+
   return {
-    base: "./",
+    base: isGitHubActions ? "/kitchen/" : "/",
     build: {
       outDir: "build",
     },
